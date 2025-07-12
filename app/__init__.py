84748007f34904515f43b0931667a58cb4968ec6
jsker-thanh <thanhnhutrong@gmail.com>
2025-07-12 12:45:40 +0700

    update basic app

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,5 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask # create object Flask from package Flask
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__) # variables predefine
 
-    # dang ky cac route tu route.py
-    from .routes import main
-    app.register_blueprint(main)
-
-    return app
-
-# app = Flask(__name__)
-# ## /// = relative path, //// = absolute path
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.lite'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
+from app import routes # import package routes, nen import o cuoi vi can app variable
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,6 @@
-from flask import Blueprint, render_template
+from app import app
 
-# khoi tao blueprint
-main = Blueprint('main', __name__)
-
-@main.route('/')
-def home():
-    return render_template('index.html')
-
-@main.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
