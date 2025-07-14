ccd615b53b948e6eea0c836745cf2b2355d5f303
jsker-thanh <thanhnhutrong@gmail.com>
2025-07-15 01:03:40 +0700

    update login form

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask # create object Flask from package Flask
+from config import Config
 
 app = Flask(__name__) # variables predefine
+app.config.from_object(Config)
 
 from app import routes # import package routes, nen import o cuoi vi can app variable
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,28 @@
 from app import app
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username' : 'Miguel'}
+    posts = [
+        {
+            'author' : {'username' : 'John'},
+            'body' : 'Hello everyone'
+        },
+        {
+            'author' : {'username' : 'hiyen'},
+            'body' : 'Hello Thanh, i miss u so much.'
+        }
+    ]
+    # return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', user=user, posts=posts)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login request for user{}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title="Login Page", form=form)
