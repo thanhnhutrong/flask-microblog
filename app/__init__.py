b8451d47d788facff286590945bdd8870ed0faac
jsker-thanh <thanhnhutrong@gmail.com>
2025-07-15 23:16:53 +0700

    update database

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask # create object Flask from package Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__) # variables predefine
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes # import package routes, nen import o cuoi vi can app variable
+from app import routes, models # import package routes, nen import o cuoi vi can app variable
--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__)) # lay duong dan toi file ma nguon hien tai -> ching la folder to-do-list-flask
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
