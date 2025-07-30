b123a91e43658d0b4bfebdacce1123573031a910
thanhnhutrong <thanhnhutrong@gmail.com>
2025-07-30 09:06:59 +0700

    fix duplicate username problem

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 app = Flask(__name__) # variables predefine
 app.config.from_object(Config)
@@ -25,10 +26,19 @@ if not app.debug:
         mail_handler = SMTPHandler(
             mailhost = (app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             # fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            fromaddr='22028219@vnu.edu.vn',
+            fromaddr=app.config['VERIFYEMAIL'],
             toaddrs=app.config['ADMINS'], subject='Microblog Failure',
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10) # thiet lap, moi ban ghi se la 10k byte, neu vuot qua se ghi sang ban khac, khong qua 10 ban
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')) # thiet lap format de log de doc hon: thoi gian, message, level, file va so dong sinh ra log
+    file_handler.setLevel(logging.INFO) # chi ghi nhung log o muc do INFO tro len (INFO, WARNING, ERROR, CRITICAL)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO) # cho phep log o muc do INFO tro len tren toan bo log cua flask app
+    app.logger.info('Microblog startup') # ghi vao dau log
 
 from app import routes, models, errors # import package routes, nen import o cuoi vi can app variable
--- config.py
+++ config.py
@@ -15,4 +15,5 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['thanhnhutrong@gmail.com']
+    ADMINS = os.environ.get('ADMINS')
+    VERIFYEMAIL = os.environ.get('VERIFYEMAIL')
